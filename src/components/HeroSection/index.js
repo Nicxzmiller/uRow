ef7c51feb6c921d4c7648a70686bd29bb6b06f15
Nicholas Markus <nicxzmiller2@gmail.com>
2021-02-04 16:26:46 +0100

    Hero

---
+++ src/components/HeroSection/index.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Video from '../../videos/video.mp4'
+import { HeroContainer, HeroBg, VideoBg} from "./HeroElements";
+
+const HeroSection = () => {
+    return(
+        <HeroContainer>
+            <HeroBg>
+                <VideoBg autoPlay loop muted src={Video} type='video/mp4'/>
+            </HeroBg>
+            <HeroContent>
+                <HeroH1>Virtual Banking Made Easy</HeroH1>
+                <HeroP>
+                    Sign up for a new account today and receive £250 in credit towards your next payment
+                </HeroP>
+                <HeroBtnWrapper>
+                    <Button to='signup'>
+                        Get started {hover ? <ArrowForward/> : <ArrowRight/>}
+                    </Button>
+                </HeroBtnWrapper>
+            </HeroContent>
+        </HeroContainer>
+    )
+}
+
+export default HeroSection;
