05d66ff49bb258e05f8f96018ee72a6fdbdbb28e
Nicholas Markus <nicxzmiller2@gmail.com>
2021-02-05 10:29:12 +0100

    Hero section

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Video from '../../videos/video.mp4'
-import { HeroContainer, HeroBg, VideoBg} from "./HeroElements";
+import { HeroContainer, HeroBg, VideoBg, HeroBtnWrapper,HeroContent, ArrowForward, ArrowRight, HeroH1, HeroP} from "./HeroElements";
+import { Button } from "../ButtonElement";
 
 const HeroSection = () => {
+
+    const [hover, setHover ] = useState(false)
+
+    const onHover = () => {
+        setHover(!hover)
+    }
     return(
         <HeroContainer>
             <HeroBg>
@@ -14,7 +21,7 @@ const HeroSection = () => {
                     Sign up for a new account today and receive £250 in credit towards your next payment
                 </HeroP>
                 <HeroBtnWrapper>
-                    <Button to='signup'>
+                    <Button to='signup' onMouseEnter={onHover} onMouseLeave={onHover} primary='true' dark='true'>
                         Get started {hover ? <ArrowForward/> : <ArrowRight/>}
                     </Button>
                 </HeroBtnWrapper>
