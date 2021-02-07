b1645c699c9a99c6725fe7c2785d920965ddabc5
Nicholas <nicxzmiller2@gmail.com>
2021-02-07 23:47:54 +0100

    Information section

--- src/components/InfoSection/Data.js
+++ src/components/InfoSection/Data.js
@@ -16,3 +16,39 @@ export const homeObjOne = {
     primary:true,
     darkText:false
 }
+
+export const homeObjTwo = {
+    id: 'discover',
+    lightBg: true,
+    lightText: false,
+    lightTextDesc:false,
+    topLine: 'Unlimited Access',
+    headline: 'Login to your account at any time',
+    description: 'We have you covered no matter where you are located. All you need' +
+        ' is an internet connection and a Phone or a Computer.',
+    buttonLabel: 'Learn More',
+    imgStart: true,
+    img: "/images/svg-2.svg",
+    alt:'piggyBank',
+    dark: false,
+    primary:false,
+    darkText:true
+}
+
+export const homeObjThree = {
+    id: 'signup',
+    lightBg: true,
+    lightText: false,
+    lightTextDesc:false,
+    topLine: 'Join our Team',
+    headline: 'Creating an account will only take minutes',
+    description: 'Get everything set and ready in 5 minutes. All you need to do is add your information and you ' +
+        'are ready to start banking',
+    buttonLabel: 'Start Now',
+    imgStart: false,
+    img: "/images/svg-3.svg",
+    alt:'Paper',
+    dark: false,
+    primary:false,
+    darkText:true
+}
